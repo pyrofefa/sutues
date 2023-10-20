c7868ad695c3c1569d5f20682208d0fe659347e2
Siafeson <siafeson@Mac-mini-de-Siafeson.local>
2023-10-20 13:49:18 -0700

    Application Procedure description

--- resources/js/Pages/Web/Transparency/ApplicationProcedure.vue
+++ resources/js/Pages/Web/Transparency/ApplicationProcedure.vue
@@ -16,5 +16,29 @@
             </div>
         </div>
     </div>
+    <div class="about-section section-padding">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-xl-6 col-lg-6 col-md-7">
+                    <div class="about-content-wrap">
+                        <div class="section-title">
+                            <h3>Datos que debe contener la solicitud:</h3>
+                        </div>
+                        <div class="single-team-item">
+                            <h4>* Nombre del solicitante. </h4>
+                            <h4>* Descripción de la información solicitada. </h4>
+                            <h4>* Lugar o medio para recibir la información. </h4>
+                            <h4>* El acceso a la información es gratuito. Cuando sea necesario o requerido proporcionarla a través de copias simples, certificados o formatos electrónicos el solicitante deberá cubrir el pago correspondiente.</h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-6 col-lg-6 col-md-5">
+                    <div class="about-bg-wrapper">
+                        <img alt="our project design" src="/assets/img/history.png">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     <Footer />
 </template>
