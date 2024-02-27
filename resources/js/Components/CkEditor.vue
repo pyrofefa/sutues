440bd074f9c781e5b8a4fed7981e278de81f2ff5
estebanCesave <francisco.flores@cesaveson.com>
2024-02-26 22:10:13 -0700

    Add link in ckeditor

--- resources/js/Components/CkEditor.vue
+++ resources/js/Components/CkEditor.vue
@@ -21,6 +21,7 @@ export default {
                     '|',
                     'bold',
                     'italic',
+                    'link',
                     '|',
                     'bulletedList',
                     'numberedList',
